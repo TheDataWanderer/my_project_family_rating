0e813ace44061cac254064705bc978084b61f611
TheDataWanderer <nefatovmaksim@gmail.com>
2025-11-20 00:40:35 +0300

    Закончил с 4-м видео

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 def index(request):
     return HttpResponse("Страница приложения women")
+
+def categories(request, catid):
+    if(request.GET): 
+        print(request.GET)
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+def arhive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=False)
+    
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
